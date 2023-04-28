07dafed1917ffc1fbe040cc7abba6e9b3ce1e6ee
nmsaini <nmsaini@gmail.com>
2023-04-28 12:30:04 -0400

    Update Hello.java
    added timestamp of image build to the greetings

--- src/main/java/com/liberty/samples/rest/Hello.java
+++ src/main/java/com/liberty/samples/rest/Hello.java
@@ -9,11 +9,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @WebServlet(urlPatterns = "/hello")
 public class Hello extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+        private static final long serialVersionUID = 1L;
+        public static final SimpleDateFormat dtFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.getWriter().append("Hello from " + Inet4Address.getLocalHost().getHostName());
-	}
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                resp.getWriter().append("Hello from " + Inet4Address.getLocalHost().getHostName() + " [built-on: "+dtFormatter.format(new Date())+"]");
+        }
 }
